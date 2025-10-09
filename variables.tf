225e8f6f994ee54f2f0b8f7bb2813db83cc3cc16
bobd50 <galavu10@gmail.com>
2025-10-09 11:44:09 +0300

    ci: fix TF_VAR exports; add safe defaults + silence unused vars for network-only

--- variables.tf
+++ variables.tf
@@ -37,35 +37,42 @@ variable "enable_nat_gateway" {
 
 
 # --- Project / Environment ---
-variable "project_name" { type = string }
-variable "environment" {
-  type    = string
-  default = "prod"
-}
+# variable "project_name" { type = string }
+# variable "environment" {
+#   type    = string
+#   default = "prod"
+# }
 
-# --- Architecture / Instance ---
-variable "arch" {
-  type    = string
-  default = "arm64"
-} # "amd64" for x86
-variable "instance_type" {
-  type    = string
-  default = "t4g.small"
-} # t3.small if amd64
+# # --- Architecture / Instance ---
+# # tflint-ignore: terraform_unused_declarations
+# variable "arch" {
+#   type    = string
+#   default = "arm64"
+# } # "amd64" for x86
 
-# --- Volumes / EIP / Paths ---
-variable "root_volume_size" {
-  type    = number
-  default = 20
-}
-variable "use_eip" {
-  type    = bool
-  default = false
-}
-variable "stack_dir" {
-  type    = string
-  default = "/opt/webstack"
-}
+# # tflint-ignore: terraform_unused_declarations
+# variable "instance_type" {
+#   type    = string
+#   default = "t4g.small"
+# } # t3.small if amd64
+
+# # --- Volumes / EIP / Paths ---
+# # tflint-ignore: terraform_unused_declarations
+# variable "root_volume_size" {
+#   type    = number
+#   default = 20
+# }
+# # tflint-ignore: terraform_unused_declarations
+# variable "use_eip" {
+#   type    = bool
+#   default = false
+# }
+
+# # tflint-ignore: terraform_unused_declarations
+# variable "stack_dir" {
+#   type    = string
+#   default = "/opt/webstack"
+# }
 
 # --- VPC / Subnets / NAT ---
 variable "vpc_cidr" { type = string }
@@ -74,22 +81,26 @@ variable "azs" {
   default = ["us-east-1a", "us-east-1b"]
 } # empty => auto-pick first 2 AZs
 
-# --- Postgres (local docker by default; password optional) ---
-variable "postgres_user" {
-  type    = string
-  default = "postgres"
-}
-variable "postgres_password" {
-  type      = string
-  default   = ""
-  sensitive = true
-}
-variable "postgres_default_db" {
-  type    = string
-  default = ""
-}
+# # --- Postgres (local docker by default; password optional) ---
+# # tflint-ignore: terraform_unused_declarations
+# variable "postgres_user" {
+#   type    = string
+#   default = "postgres"
+# }
+# # tflint-ignore: terraform_unused_declarations
+# variable "postgres_password" {
+#   type      = string
+#   default   = ""
+#   sensitive = true
+# }
+# # tflint-ignore: terraform_unused_declarations
+# variable "postgres_default_db" {
+#   type    = string
+#   default = ""
+# }
+
 
 variable "ssh_allowed_cidr" {
-  description = "CIDR allowed to SSH (e.g., 1.2.3.4/32)"
-  type        = string
+  type    = string
+  default = "197.248.148.214/32"
 }
